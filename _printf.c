d5ccebdc7be3e0287a99c2f8244f6a597bde9096
TemoTech25 <temotech25@gmail.com>
2023-11-13 22:42:16 -0800

    group project

--- _printf.c
+++ _printf.c
@@ -8,32 +8,32 @@
  */
 int printf_helper(int num)
 {
-    int count = 0;
-    int temp, len;
+	int count = 0;
+	int temp, len;
 
-    if (num < 0)
-    {
-        count += putchar('-');
-        num *= -1;
-    }
+	if (num < 0)
+	{
+		count += putchar('-');
+		num *= -1;
+	}
 
-    temp = num;
-    while (temp != 0)
-    {
-        temp /= 10;
-        count++;
-    }
+	temp = num;
+	while (temp != 0)
+	{
+		temp /= 10;
+		count++;
+	}
 
-    temp = num;
-    while (temp != 0)
-    {
-        len = temp % 10;
-        putchar(len + '0');
-        temp /= 10;
-        count++;
-    }
+	temp = num;
+	while (temp != 0)
+	{
+		len = temp % 10;
+		putchar(len + '0');
+		temp /= 10;
+		count++;
+	}
 
-    return (count);
+	return (count);
 }
 
 /**
@@ -42,64 +42,62 @@ int printf_helper(int num)
  *
  * Return: The number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i, count = 0;
+	va_list args;
+	int i, count = 0;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    for (i = 0; format && format[i]; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            switch (format[i])
-            {
-                case 'c':
-                {
-                    char c = va_arg(args, int);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+				{
+					char c = va_arg(args, int);
 
-                    count += putchar(c);
-                    break;
-                }
-                case 's':
-                {
-                    char *s = va_arg(args, char*);
+					count += putchar(c);
+					break;
+				}
+				case 's':
+				{
+					char *s = va_arg(args, char*);
 
-                    while (*s)
-                    {
-                        putchar(*s);
-                        s++;
-                        count++;
-                    }
-                    break;
-                }
-                case 'd':
-                {
-                    int num = va_arg(args, int);
+					while (*s)
+					{
+						putchar(*s);
+						s++;
+						count++;
+					}
+					break;
+				}
+				case 'd':
+				{
+					int num = va_arg(args, int);
 
-                    if (num < 0)
-                    {
-                        count += putchar('-');
-                        num *= -1;
-                    }
-                    count += printf_helper(num);
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
-        else
-        {
-            putchar(format[i]);
-            count++;
-        }
-    }
-
-    va_end(args);
-
-    return (count);
+					if (num < 0)
+					{
+						count += putchar('-');
+						num *= -1;
+					}
+					count += printf_helper(num);
+					break;
+				}
+				default:
+					break;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
-
