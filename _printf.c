33df9001a9acb112f5085186cb90d9cce98cea29
TemoTech25 <temotech25@gmail.com>
2023-11-13 22:00:13 -0800

    group project

--- _printf.c
+++ _printf.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ * printf_helper - Helper function for _printf
+ * @num: The number to print
+ *
+ * Return: The number of characters printed
+ */
+int printf_helper(int num)
+{
+    int count = 0;
+    int temp, len;
+
+    if (num < 0)
+    {
+        count += putchar('-');
+        num *= -1;
+    }
+
+    temp = num;
+    while (temp != 0)
+    {
+        temp /= 10;
+        count++;
+    }
+
+    temp = num;
+    while (temp != 0)
+    {
+        len = temp % 10;
+        putchar(len + '0');
+        temp /= 10;
+        count++;
+    }
+
+    return (count);
+}
+
+/**
  * _printf - Prints the given format string
  * @format: The format string
  *
@@ -66,3 +102,4 @@ int _printf(const char *format, ...)
 
     return (count);
 }
+
