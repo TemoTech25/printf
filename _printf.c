e0b3a309a91dd94bec38ca90288e4fdb68efd842
TemoTech25 <temotech25@gmail.com>
2023-11-13 20:51:54 -0800

    group project

--- _printf.c
+++ _printf.c
@@ -1,64 +1,53 @@
 #include "main.h"
 
 /**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
-/**
  * _printf - Custom printf function
  * @format: Format string
- * Return: Number of printed chars
+ * Return: Number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, output_buff_ind = 0;
-	va_list list;
-	char output_buffer[BUFF_SIZE];
+    if (format == NULL)
+        return (-1);
 
-	if (format == NULL)
-		return (-1);
+    int printed_chars = 0;
+    va_list list;
 
-	va_start(list, format);
+    va_start(list, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			output_buffer[output_buff_ind++] = format[i];
-			if (output_buff_ind == BUFF_SIZE)
-				print_buffer(output_buffer, &output_buff_ind);
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(output_buffer, &output_buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, output_buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+    for (int i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
+        {
+            write(1, &format[i], 1);
+            printed_chars++;
+        }
+        else
+        {
+            i++; // Move to the next character after '%'
+            switch (format[i])
+            {
+            case 'c':
+                printed_chars += write_char(va_arg(list, int));
+                break;
+            case 's':
+                printed_chars += write_str(va_arg(list, char *));
+                break;
+            case '%':
+                write(1, "%", 1);
+                printed_chars++;
+                break;
+            default:
+                write(1, "%", 1);
+                write(1, &format[i], 1);
+                printed_chars += 2;
+                break;
+            }
+        }
+    }
 
-	print_buffer(output_buffer, &output_buff_ind);
+    va_end(list);
 
-	va_end(list);
-
-	return (printed_chars);
+    return printed_chars;
 }
 
