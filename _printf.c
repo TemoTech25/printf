4d405532a39ffea7fb31bfae87fb567ee8a41c3c
TemoTech25 <temotech25@gmail.com>
2023-11-13 21:20:17 -0800

    group project

--- _printf.c
+++ _printf.c
@@ -1,63 +1,68 @@
 #include "main.h"
 
 /**
- * print_buffer - Prints the contents of the buffer if it exists
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
- */
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
-}
-
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Prints the given format string
+ * @format: The format string
+ *
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
+    va_list args;
+    int i, count = 0;
 
-	if (format == NULL)
-		return (-1);
+    va_start(args, format);
 
-	va_start(list, format);
+    for (i = 0; format && format[i]; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'c':
+                {
+                    char c = va_arg(args, int);
 
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
+                    count += putchar(c);
+                    break;
+                }
+                case 's':
+                {
+                    char *s = va_arg(args, char*);
 
-	print_buffer(buffer, &buff_ind);
+                    while (*s)
+                    {
+                        putchar(*s);
+                        s++;
+                        count++;
+                    }
+                    break;
+                }
+                case 'd':
+                {
+                    int num = va_arg(args, int);
 
-	va_end(list);
+                    if (num < 0)
+                    {
+                        count += putchar('-');
+                        num *= -1;
+                    }
+                    count += printf_helper(num);
+                    break;
+                }
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            putchar(format[i]);
+            count++;
+        }
+    }
 
-	return (printed_chars);
+    va_end(args);
+
+    return (count);
 }
