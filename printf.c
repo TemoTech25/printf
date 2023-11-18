29bc820e3daa564049208661bf235013f6cd6885
LesegoSE <Lesego.seikali@gmail.com>
2023-11-17 16:38:18 -0800

    lesego

--- printf.c
+++ printf.c
@@ -3,15 +3,15 @@
 /**
  * _printf - a function that produces output according to a format.
  * @format: argument
- *
  * Return: The number of characters printed.
  */
+
 int _printf(const char *format, ...)
 {
 	convert p[] = {
 		{"%s", output_string}, {"%c", output_char},
 		{"%%", output_percent}, {"%i", output_int},
-		{"%d", output_int}, {"%r", output_reversed_string},
+		{"%d", output_decimal}, {"%r", output_reversed_string},
 		{"%R", output_rot13}, {"%b", output_binary},
 		{"%u", output_unsigned},
 		{"%o", output_octal}, {"%x", output_hex}, {"%X", output_hex_uppercase},
@@ -42,7 +42,7 @@ Here:
 		{
 			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
 			{
-				length += p[j].function(args);
+				length = length + p[j].function(args);
 				i = i + 2;
 				goto Here;
 			}
